b3f8593b56a49d6672729eabf702d8d643bce143
alexandsomeadventures <educationfor300@gmail.com>
2024-09-29 04:34:38 -0700

    changed to gpt-4o model instead of LLAMA for better results. added image content besides search query for LLM

--- startbootstrap-modern-business-gh-pages/flask_app/app.py
+++ startbootstrap-modern-business-gh-pages/flask_app/app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template, url_for, request, redirect, send_from_directory, jsonify
-from together import Together
+import openai
 import base64
 from dotenv import load_dotenv
 app = Flask(__name__)
@@ -26,20 +26,34 @@ def upload():
     file = request.files['file']
     search_query = request.form.get("search_query")
     encoded_image = base64.b64encode(file.read()).decode('utf-8') 
-    
-    client = Together(api_key=os.getenv("TOGETHER_API_KEY"))
-    stream = client.chat.completions.create(
-        model="meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo",
-        messages=[{"role": "user", "content": search_query}],
-        stream=True,
+    base64_string = f"data:{file.content_type};base64,{encoded_image}"
+    openai.api_key = os.getenv("OPENAI_API_KEY")  # Set the API key directly
+    completion = openai.ChatCompletion.create(
+        model="gpt-4o",
+        messages=[
+            {
+                "role": "system",
+                "content": '''
+                You are a virtual sales assistant for Amazon where you assist customers in picking out new clothes. The photo is one of your customer. The customer will make a request. Determine an outfit that satisfies the request. Determine if the photo is upper body only. If it is, suggest a single Amazon query to find a {"top":?} that matches the outfit. If it is not, suggest Amazon queries for each of the following to find clothing for the new outfit: {"top":?, "bottom":?}, {"bottom":?}, {"top":}, or {"dress":?}. Select the most appropriate query section. After generating the reply, begin a code block and format the queries and upper_body_only as json. Be sure to include the article of clothing as the key and query as the value for the query section.
+                '''
+            },
+            {
+                "role": "user",
+                "content": [
+                    search_query,
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": base64_string
+                        }
+                    }]
+            }
+        ]
     )
     # Process and print the response from LLaMA
-    response_content = ""
-    for chunk in stream:
-        response_content += chunk.choices[0].delta.content or ""
     print(search_query)
     print(encoded_image)
-    print(response_content)
+    print(completion.choices[0].message)
     return jsonify({
         "encoded_image": encoded_image,
         "search_query": search_query,
